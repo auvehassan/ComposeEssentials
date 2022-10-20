6f9c73c8f75474f38b0923ba24011cded6245f46
Md. Atikul Hassan <auvehassan@gmail.com>
2022-10-20 09:46:59 +0600

    Using a Column

--- app/src/main/java/com/auvehassan/composeessentials/MainActivity.kt
+++ app/src/main/java/com/auvehassan/composeessentials/MainActivity.kt
@@ -3,12 +3,9 @@ package com.auvehassan.composeessentials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.auvehassan.composeessentials.ui.theme.ComposeEssentialsTheme
 
@@ -31,13 +28,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Message(val author:String, val body:String)
+data class Message(val author: String, val body: String)
 
 //To make a function composable, add the @Composable annotation
 @Composable
 fun Greeting(message: Message) {
-    Text(text = message.author)
-    Text(text = message.body)
+    Column {//The Column function lets you arrange elements vertically.
+        Text(text = message.author)
+        Text(text = message.body)
+    }
 }
 
 //The @Preview annotation lets you preview your composable functions
