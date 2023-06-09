645352a5fb995d1f22d4bdc84eb4e6386935e8a3
Md. Atikul Hassan <auvehassan@gmail.com>
2023-06-10 02:24:23 +0600

    starter completed

--- app/src/main/java/com/auvehassan/composeessentials/MainActivity.kt
+++ app/src/main/java/com/auvehassan/composeessentials/MainActivity.kt
@@ -3,28 +3,62 @@ package com.auvehassan.composeessentials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.auvehassan.composeessentials.ui.theme.ComposeEssentialsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var messages: MutableList<Message> = ArrayList()
+
+    init {
+        messages.add(Message("TestAuthor", "TestBody"))
+        messages.add(Message("TestAuthor TestAuthor ", "TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody TestBody "))
+        messages.add(Message("TestAuthor TestAuthor TestAuthor ", "TestBody"))
+        messages.add(Message("TestAuthor TestAuthor ", "TestBody"))
+        messages.add(Message("TestAuthor TestAuthor TestAuthor TestAuthor TestAuthor ", "TestBody"))
+        messages.add(Message("TestAuthor", "TestBody"))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(Message("TestAuthor", "TestBody"))
+//            Greeting(Message("TestAuthor", "TestBody"))
 //            Text(text = "Hello android")
-//            ComposeEssentialsTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            ComposeEssentialsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    MessageCard(Message("TestAuthor", "TestBody"))
+                    Conversation(messages)
+                }
+            }
         }
     }
 }
@@ -33,14 +67,45 @@ data class Message(val author: String, val body: String)
 
 //To make a function composable, add the @Composable annotation
 @Composable
-fun Greeting(message: Message) {
-    Column {//The Column function lets you arrange elements vertically.
-        Text(text = message.author)
-        Text(text = message.body)
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_sample),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
 
-        Row {//Row to arrange items horizontally
-            Text(text = message.author)
-            Text(text = message.body)
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.h6
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
@@ -50,8 +115,21 @@ fun Greeting(message: Message) {
 @Composable
 fun DefaultPreview() {
     ComposeEssentialsTheme {
-        Greeting(Message("Android Author", "Test Body"))
+        MessageCard(Message("Android Author", "Test Body"))
     }
 
+    //PreviewConversation
+   /* ComposeTutorialTheme {
+            Conversation(SampleData.conversationSample)
+        }*/
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
 }
 
