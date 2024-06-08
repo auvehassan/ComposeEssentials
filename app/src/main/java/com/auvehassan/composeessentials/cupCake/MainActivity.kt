bf10272632d2a5977f09dcbc6564267696a4c76c
Md. Atikul Hassan (Auve) <atikul.hassan@revesoft.com>
2024-06-08 21:21:08 +0600

    cupcake codelab

--- app/src/main/java/com/auvehassan/composeessentials/cupCake/MainActivity.kt
+++ app/src/main/java/com/auvehassan/composeessentials/cupCake/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.auvehassan.composeessentials.cupCake.ui.theme.CupcakeTheme
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            CupcakeApp()
+            CupcakeTheme {
+                CupcakeApp()
+            }
         }
     }
 }
